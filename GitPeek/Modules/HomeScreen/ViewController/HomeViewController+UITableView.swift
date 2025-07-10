a0d402bc6b9c442c35985b116314b3ee219a84f0
Abhijeet Ranjan <104762063+abhijeet030@users.noreply.github.com>
2025-07-10 12:02:57 +0530

    handled index out of range crash

--- GitPeek/Modules/HomeScreen/ViewController/HomeViewController+UITableView.swift
+++ GitPeek/Modules/HomeScreen/ViewController/HomeViewController+UITableView.swift
@@ -14,13 +14,17 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard indexPath.row < viewModel.users.count else {
+            return UITableViewCell()
+        }
+
         let user = viewModel.users[indexPath.row]
+
         if user.isBookmarked {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: KnownUserTableViewCell.identifier, for: indexPath) as? KnownUserTableViewCell else {
                 return UITableViewCell()
             }
             cell.configure(with: user)
-            
             cell.onBookmarkTapped = { isBookmarked in
                 if isBookmarked {
                     self.viewModel.saveBookMarkedUser(user)
@@ -34,7 +38,6 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
                 return UITableViewCell()
             }
             cell.configure(with: user)
-            
             cell.onBookmarkTapped = { isBookmarked in
                 if isBookmarked {
                     self.viewModel.saveBookMarkedUser(user)
@@ -45,9 +48,12 @@ extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
             return cell
         }
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+
+        guard indexPath.row < viewModel.users.count else { return }
+
         let selectedUser = viewModel.users[indexPath.row]
         let detailVC = UserDetailViewController(user: selectedUser)
         navigationController?.pushViewController(detailVC, animated: true)
